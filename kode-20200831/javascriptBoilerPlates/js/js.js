e4cf17f3d5de69d6b5d56749bd7a73ce14885d1c
Mae-Berces <eaajmmb@students.eaaa.dk>
2020-08-31 10:14:29 +0200

    javascript tutorial

--- kode-20200831/javascriptBoilerPlates/js/js.js
+++ kode-20200831/javascriptBoilerPlates/js/js.js
@@ -4,9 +4,21 @@
  **/
 console.log('JavaScript from js/js.js: up and running!');
 
-// Tjek at DOM er indlæst
-window.onload(function () {
-    // ... your code here ...
-    
-    // ... end ...
-});
+document.write("<h1>Hej verden</h1>");
+/**
+ *JavaScript Byggekoldser
+ **/
+
+
+//gammeldags måde at skrive var på
+var magi = "Hokus poukus filiokus filihankat!";
+var markup= "<strong>";
+var markupX= "</stron>";
+
+//mere moderne måde til var er
+let citat = ' "Jylland mellem tvede have." ';
+
+//hvis en værdie ikke ændres
+const navn = "<br>H.C Anderesen ( 1885-1875)"
+
+document.write(markup + citat + navn + markupX);
